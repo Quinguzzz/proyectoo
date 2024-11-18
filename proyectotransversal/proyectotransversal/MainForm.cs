267fceb37d6101da5cd8d83973ed1d2d615d6286
Quinguzzz <quga081028mgtnzra2@cbtis217.edu.mx>
2024-11-17 18:55:18 -0600

    Creación

---
+++ proyectotransversal/proyectotransversal/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: quing
+ * Date: 17/11/2024
+ * Time: 06:52 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyectotransversal
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
