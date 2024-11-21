1905b2b0ec402f43b29774ceb30fff55cdb94bc4
Quinguzzz <quga081028mgtnzra2@cbtis217.edu.mx>
2024-11-20 18:09:21 -0600

    Código 3

--- proyectotransversal/proyectotransversal/reporte.Designer.cs
+++ proyectotransversal/proyectotransversal/reporte.Designer.cs
@@ -47,26 +47,26 @@ namespace proyectotransversal
 			this.label7 = new System.Windows.Forms.Label();
 			this.label10 = new System.Windows.Forms.Label();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel2 = new System.Windows.Forms.Panel();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.lblTamano = new System.Windows.Forms.Label();
-			this.lblAserrin = new System.Windows.Forms.Label();
-			this.label11 = new System.Windows.Forms.Label();
-			this.lblManodeobra = new System.Windows.Forms.Label();
-			this.label13 = new System.Windows.Forms.Label();
-			this.label14 = new System.Windows.Forms.Label();
-			this.lblPintura = new System.Windows.Forms.Label();
-			this.lblFrutos = new System.Windows.Forms.Label();
-			this.label17 = new System.Windows.Forms.Label();
-			this.lblDiseno = new System.Windows.Forms.Label();
-			this.label19 = new System.Windows.Forms.Label();
-			this.lblAgua = new System.Windows.Forms.Label();
-			this.label21 = new System.Windows.Forms.Label();
-			this.lblFlores = new System.Windows.Forms.Label();
-			this.label23 = new System.Windows.Forms.Label();
 			this.lblTotal = new System.Windows.Forms.Label();
 			this.label25 = new System.Windows.Forms.Label();
+			this.lblFlores = new System.Windows.Forms.Label();
+			this.label23 = new System.Windows.Forms.Label();
+			this.lblAgua = new System.Windows.Forms.Label();
+			this.label21 = new System.Windows.Forms.Label();
+			this.lblDiseno = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.lblFrutos = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.lblPintura = new System.Windows.Forms.Label();
+			this.lblManodeobra = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.lblAserrin = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.lblTamano = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
@@ -214,10 +214,190 @@ namespace proyectotransversal
 			this.panel1.Controls.Add(this.label11);
 			this.panel1.Controls.Add(this.lblTamano);
 			this.panel1.Controls.Add(this.label5);
-			this.panel1.Location = new System.Drawing.Point(61, 216);
+			this.panel1.Location = new System.Drawing.Point(45, 213);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(735, 336);
+			this.panel1.Size = new System.Drawing.Size(765, 336);
 			this.panel1.TabIndex = 130;
+			// 
+			// lblTotal
+			// 
+			this.lblTotal.AutoSize = true;
+			this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTotal.Location = new System.Drawing.Point(330, 321);
+			this.lblTotal.Name = "lblTotal";
+			this.lblTotal.Size = new System.Drawing.Size(29, 33);
+			this.lblTotal.TabIndex = 15;
+			this.lblTotal.Text = "0";
+			// 
+			// label25
+			// 
+			this.label25.AutoSize = true;
+			this.label25.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label25.Location = new System.Drawing.Point(28, 321);
+			this.label25.Name = "label25";
+			this.label25.Size = new System.Drawing.Size(113, 33);
+			this.label25.TabIndex = 14;
+			this.label25.Text = "TOTAL:";
+			// 
+			// lblFlores
+			// 
+			this.lblFlores.AutoSize = true;
+			this.lblFlores.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFlores.Location = new System.Drawing.Point(681, 249);
+			this.lblFlores.Name = "lblFlores";
+			this.lblFlores.Size = new System.Drawing.Size(28, 31);
+			this.lblFlores.TabIndex = 13;
+			this.lblFlores.Text = "0";
+			// 
+			// label23
+			// 
+			this.label23.AutoSize = true;
+			this.label23.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label23.Location = new System.Drawing.Point(28, 249);
+			this.label23.Name = "label23";
+			this.label23.Size = new System.Drawing.Size(225, 31);
+			this.label23.TabIndex = 12;
+			this.label23.Text = "MANO DE OBRA:";
+			// 
+			// lblAgua
+			// 
+			this.lblAgua.AutoSize = true;
+			this.lblAgua.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAgua.Location = new System.Drawing.Point(678, 193);
+			this.lblAgua.Name = "lblAgua";
+			this.lblAgua.Size = new System.Drawing.Size(28, 31);
+			this.lblAgua.TabIndex = 11;
+			this.lblAgua.Text = "0";
+			// 
+			// label21
+			// 
+			this.label21.AutoSize = true;
+			this.label21.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label21.Location = new System.Drawing.Point(388, 193);
+			this.label21.Name = "label21";
+			this.label21.Size = new System.Drawing.Size(237, 31);
+			this.label21.TabIndex = 10;
+			this.label21.Text = "LITROS DE AGUA:";
+			// 
+			// lblDiseno
+			// 
+			this.lblDiseno.AutoSize = true;
+			this.lblDiseno.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDiseno.Location = new System.Drawing.Point(678, 131);
+			this.lblDiseno.Name = "lblDiseno";
+			this.lblDiseno.Size = new System.Drawing.Size(28, 31);
+			this.lblDiseno.TabIndex = 9;
+			this.lblDiseno.Text = "0";
+			// 
+			// label19
+			// 
+			this.label19.AutoSize = true;
+			this.label19.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label19.Location = new System.Drawing.Point(28, 193);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(213, 31);
+			this.label19.TabIndex = 8;
+			this.label19.Text = "FRUTOS SECOS:";
+			// 
+			// lblFrutos
+			// 
+			this.lblFrutos.AutoSize = true;
+			this.lblFrutos.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFrutos.Location = new System.Drawing.Point(331, 193);
+			this.lblFrutos.Name = "lblFrutos";
+			this.lblFrutos.Size = new System.Drawing.Size(28, 31);
+			this.lblFrutos.TabIndex = 7;
+			this.lblFrutos.Text = "0";
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label17.Location = new System.Drawing.Point(388, 249);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(120, 31);
+			this.label17.TabIndex = 6;
+			this.label17.Text = "FLORES:";
+			// 
+			// lblPintura
+			// 
+			this.lblPintura.AutoSize = true;
+			this.lblPintura.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPintura.Location = new System.Drawing.Point(331, 131);
+			this.lblPintura.Name = "lblPintura";
+			this.lblPintura.Size = new System.Drawing.Size(28, 31);
+			this.lblPintura.TabIndex = 5;
+			this.lblPintura.Text = "0";
+			// 
+			// lblManodeobra
+			// 
+			this.lblManodeobra.AutoSize = true;
+			this.lblManodeobra.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblManodeobra.Location = new System.Drawing.Point(331, 249);
+			this.lblManodeobra.Name = "lblManodeobra";
+			this.lblManodeobra.Size = new System.Drawing.Size(28, 31);
+			this.lblManodeobra.TabIndex = 5;
+			this.lblManodeobra.Text = "0";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label14.Location = new System.Drawing.Point(28, 131);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(276, 31);
+			this.label14.TabIndex = 4;
+			this.label14.Text = "LITROS DE PINTURA:";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label13.Location = new System.Drawing.Point(388, 131);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(257, 31);
+			this.label13.TabIndex = 4;
+			this.label13.Text = "HORAS DE DISEÑO:";
+			// 
+			// lblAserrin
+			// 
+			this.lblAserrin.AutoSize = true;
+			this.lblAserrin.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAserrin.Location = new System.Drawing.Point(678, 67);
+			this.lblAserrin.Name = "lblAserrin";
+			this.lblAserrin.Size = new System.Drawing.Size(28, 31);
+			this.lblAserrin.TabIndex = 3;
+			this.lblAserrin.Text = "0";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.Location = new System.Drawing.Point(388, 67);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(284, 31);
+			this.label11.TabIndex = 2;
+			this.label11.Text = "BULTOS DE ASERRIN:";
+			// 
+			// lblTamano
+			// 
+			this.lblTamano.AutoSize = true;
+			this.lblTamano.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTamano.Location = new System.Drawing.Point(331, 67);
+			this.lblTamano.Name = "lblTamano";
+			this.lblTamano.Size = new System.Drawing.Size(28, 31);
+			this.lblTamano.TabIndex = 1;
+			this.lblTamano.Text = "0";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(28, 67);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(297, 31);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "TAMAÑO DEL TAPETE:";
 			// 
 			// panel2
 			// 
@@ -243,186 +423,6 @@ namespace proyectotransversal
 			this.label4.Text = "¡Da clic aquí para volver al inicio!";
 			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			this.label4.Click += new System.EventHandler(this.Label4Click);
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label5.Location = new System.Drawing.Point(28, 67);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(297, 31);
-			this.label5.TabIndex = 0;
-			this.label5.Text = "TAMAÑO DEL TAPETE:";
-			// 
-			// lblTamano
-			// 
-			this.lblTamano.AutoSize = true;
-			this.lblTamano.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblTamano.Location = new System.Drawing.Point(331, 67);
-			this.lblTamano.Name = "lblTamano";
-			this.lblTamano.Size = new System.Drawing.Size(28, 31);
-			this.lblTamano.TabIndex = 1;
-			this.lblTamano.Text = "0";
-			// 
-			// lblAserrin
-			// 
-			this.lblAserrin.AutoSize = true;
-			this.lblAserrin.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblAserrin.Location = new System.Drawing.Point(678, 67);
-			this.lblAserrin.Name = "lblAserrin";
-			this.lblAserrin.Size = new System.Drawing.Size(28, 31);
-			this.lblAserrin.TabIndex = 3;
-			this.lblAserrin.Text = "0";
-			// 
-			// label11
-			// 
-			this.label11.AutoSize = true;
-			this.label11.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label11.Location = new System.Drawing.Point(388, 67);
-			this.label11.Name = "label11";
-			this.label11.Size = new System.Drawing.Size(284, 31);
-			this.label11.TabIndex = 2;
-			this.label11.Text = "BULTOS DE ASERRIN:";
-			// 
-			// lblManodeobra
-			// 
-			this.lblManodeobra.AutoSize = true;
-			this.lblManodeobra.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblManodeobra.Location = new System.Drawing.Point(331, 249);
-			this.lblManodeobra.Name = "lblManodeobra";
-			this.lblManodeobra.Size = new System.Drawing.Size(28, 31);
-			this.lblManodeobra.TabIndex = 5;
-			this.lblManodeobra.Text = "0";
-			// 
-			// label13
-			// 
-			this.label13.AutoSize = true;
-			this.label13.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label13.Location = new System.Drawing.Point(388, 131);
-			this.label13.Name = "label13";
-			this.label13.Size = new System.Drawing.Size(257, 31);
-			this.label13.TabIndex = 4;
-			this.label13.Text = "HORAS DE DISEÑO:";
-			// 
-			// label14
-			// 
-			this.label14.AutoSize = true;
-			this.label14.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label14.Location = new System.Drawing.Point(28, 131);
-			this.label14.Name = "label14";
-			this.label14.Size = new System.Drawing.Size(276, 31);
-			this.label14.TabIndex = 4;
-			this.label14.Text = "LITROS DE PINTURA:";
-			// 
-			// lblPintura
-			// 
-			this.lblPintura.AutoSize = true;
-			this.lblPintura.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblPintura.Location = new System.Drawing.Point(331, 131);
-			this.lblPintura.Name = "lblPintura";
-			this.lblPintura.Size = new System.Drawing.Size(28, 31);
-			this.lblPintura.TabIndex = 5;
-			this.lblPintura.Text = "0";
-			// 
-			// lblFrutos
-			// 
-			this.lblFrutos.AutoSize = true;
-			this.lblFrutos.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblFrutos.Location = new System.Drawing.Point(331, 193);
-			this.lblFrutos.Name = "lblFrutos";
-			this.lblFrutos.Size = new System.Drawing.Size(28, 31);
-			this.lblFrutos.TabIndex = 7;
-			this.lblFrutos.Text = "0";
-			// 
-			// label17
-			// 
-			this.label17.AutoSize = true;
-			this.label17.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label17.Location = new System.Drawing.Point(388, 249);
-			this.label17.Name = "label17";
-			this.label17.Size = new System.Drawing.Size(120, 31);
-			this.label17.TabIndex = 6;
-			this.label17.Text = "FLORES:";
-			// 
-			// lblDiseno
-			// 
-			this.lblDiseno.AutoSize = true;
-			this.lblDiseno.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblDiseno.Location = new System.Drawing.Point(678, 131);
-			this.lblDiseno.Name = "lblDiseno";
-			this.lblDiseno.Size = new System.Drawing.Size(28, 31);
-			this.lblDiseno.TabIndex = 9;
-			this.lblDiseno.Text = "0";
-			// 
-			// label19
-			// 
-			this.label19.AutoSize = true;
-			this.label19.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label19.Location = new System.Drawing.Point(28, 193);
-			this.label19.Name = "label19";
-			this.label19.Size = new System.Drawing.Size(213, 31);
-			this.label19.TabIndex = 8;
-			this.label19.Text = "FRUTOS SECOS:";
-			// 
-			// lblAgua
-			// 
-			this.lblAgua.AutoSize = true;
-			this.lblAgua.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblAgua.Location = new System.Drawing.Point(678, 193);
-			this.lblAgua.Name = "lblAgua";
-			this.lblAgua.Size = new System.Drawing.Size(28, 31);
-			this.lblAgua.TabIndex = 11;
-			this.lblAgua.Text = "0";
-			// 
-			// label21
-			// 
-			this.label21.AutoSize = true;
-			this.label21.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label21.Location = new System.Drawing.Point(388, 193);
-			this.label21.Name = "label21";
-			this.label21.Size = new System.Drawing.Size(237, 31);
-			this.label21.TabIndex = 10;
-			this.label21.Text = "LITROS DE AGUA:";
-			// 
-			// lblFlores
-			// 
-			this.lblFlores.AutoSize = true;
-			this.lblFlores.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblFlores.Location = new System.Drawing.Point(681, 249);
-			this.lblFlores.Name = "lblFlores";
-			this.lblFlores.Size = new System.Drawing.Size(28, 31);
-			this.lblFlores.TabIndex = 13;
-			this.lblFlores.Text = "0";
-			// 
-			// label23
-			// 
-			this.label23.AutoSize = true;
-			this.label23.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label23.Location = new System.Drawing.Point(28, 249);
-			this.label23.Name = "label23";
-			this.label23.Size = new System.Drawing.Size(225, 31);
-			this.label23.TabIndex = 12;
-			this.label23.Text = "MANO DE OBRA:";
-			// 
-			// lblTotal
-			// 
-			this.lblTotal.AutoSize = true;
-			this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.lblTotal.Location = new System.Drawing.Point(330, 321);
-			this.lblTotal.Name = "lblTotal";
-			this.lblTotal.Size = new System.Drawing.Size(29, 33);
-			this.lblTotal.TabIndex = 15;
-			this.lblTotal.Text = "0";
-			// 
-			// label25
-			// 
-			this.label25.AutoSize = true;
-			this.label25.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label25.Location = new System.Drawing.Point(28, 321);
-			this.label25.Name = "label25";
-			this.label25.Size = new System.Drawing.Size(113, 33);
-			this.label25.TabIndex = 14;
-			this.label25.Text = "TOTAL:";
 			// 
 			// reporte
 			// 
